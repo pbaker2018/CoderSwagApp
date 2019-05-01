3930959fab29f33110bd71f542375b94200f5f88
Pauline <pbaker@hotmail.co.uk>
2019-05-01 13:11:49 +0100

    Added an adapter to the MainActivity, to tell the ListView what data I want displayed.

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coderswag.Model.Category
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+    /*the adapter is what puts the data from our dataService into our listview*/
+    /*the adapter is the middle-man between the listview and the data*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        categoriesListView.adapter = adapter
+        /*this is setting the adapter for the categoriesListView as the adapter
+        we have just intialised.*/
     }
 }
