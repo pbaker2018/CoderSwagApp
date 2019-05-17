304dff5f0d7b41792a36aff306303682b3ce8915
Pauline <pbaker@hotmail.co.uk>
2019-05-17 14:04:36 +0100

    recycler view finished.

--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -15,6 +15,14 @@ object DataService {
         Product("Devslopes black beanie", "£10", "hat1"),
         Product("Devslopes black baseball cap", "£12", "hat2"),
         Product("Devslopes white baseball cap", "£12", "hat3"),
+        Product("Devslopes black/lilac baseball cap", "£12", "hat4"),
+        Product("Devslopes black beanie", "£10", "hat1"),
+        Product("Devslopes black baseball cap", "£12", "hat2"),
+        Product("Devslopes white baseball cap", "£12", "hat3"),
+        Product("Devslopes black/lilac baseball cap", "£12", "hat4"),
+        Product("Devslopes black beanie", "£10", "hat1"),
+        Product("Devslopes black baseball cap", "£12", "hat2"),
+        Product("Devslopes white baseball cap", "£12", "hat3"),
         Product("Devslopes black/lilac baseball cap", "£12", "hat4")
     )
 
@@ -22,10 +30,28 @@ object DataService {
         Product("Devslopes black zip hoodie", "£17", "hoodie1"),
         Product("Devslopes red zip hoodie", "£17", "hoodie2"),
         Product("Devslopes dark grey zip hoodie", "£17", "hoodie3"),
+        Product("Devslopes black no-zip hoodie", "£20", "hoodie4"),
+        Product("Devslopes black zip hoodie", "£17", "hoodie1"),
+        Product("Devslopes red zip hoodie", "£17", "hoodie2"),
+        Product("Devslopes dark grey zip hoodie", "£17", "hoodie3"),
+        Product("Devslopes black no-zip hoodie", "£20", "hoodie4"),
+        Product("Devslopes black zip hoodie", "£17", "hoodie1"),
+        Product("Devslopes red zip hoodie", "£17", "hoodie2"),
+        Product("Devslopes dark grey zip hoodie", "£17", "hoodie3"),
         Product("Devslopes black no-zip hoodie", "£20", "hoodie4")
     )
 
     val shirts = listOf(
+        Product("Devslopes black tee", "£13", "shirt1"),
+        Product("Devslopes grey tee", "£13", "shirt2"),
+        Product("Devslopes red tee", "£13", "shirt3"),
+        Product("Devslopes hustle tee", "£15", "shirt4"),
+        Product("Kick Flip Studios tee", "£10", "shirt5"),
+        Product("Devslopes black tee", "£13", "shirt1"),
+        Product("Devslopes grey tee", "£13", "shirt2"),
+        Product("Devslopes red tee", "£13", "shirt3"),
+        Product("Devslopes hustle tee", "£15", "shirt4"),
+        Product("Kick Flip Studios tee", "£10", "shirt5"),
         Product("Devslopes black tee", "£13", "shirt1"),
         Product("Devslopes grey tee", "£13", "shirt2"),
         Product("Devslopes red tee", "£13", "shirt3"),
